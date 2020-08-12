15e62267890c36c5cc0e3983fde7144076bc5fa7
Michael Passade <michael.passade@codeimmersives.com>
2020-08-12 13:33:09 -0400

    started Background component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-// import '../styles/App.css'
+import Background from './Background'
 
 class App extends Component {
   
   render(){
     return (
       <>
+      <Background/>
       </>
     )
   }
---
+++ src/components/Background.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Background extends Component {
+    render(){
+        return (
+            <div className='bg-imgs'>
+                <img
+                    src='/images/cover-photo.png'
+                    className='cover'
+                />
+            </div>
+        )
+    }
+}
+
+export default Background
