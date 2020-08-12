1b81e0db12bb4b0db9ae7daa17b9e3cb354c51f4
Michael Passade <michael.passade@codeimmersives.com>
2020-08-12 15:43:08 -0400

    finished header component with its css

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+
+    render(){
+        return (
+            <>
+                <header
+                    className='beto'
+                >
+                    BETO FLEE
+                </header>
+            </>
+        )   
+    }
+}
+
+export default Header
